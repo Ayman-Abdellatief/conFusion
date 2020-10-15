a071a9a193093c2dca5b67c3defb00ea765905a2
Ayman <aymn.abdellatief@gmail.com>
2020-10-15 17:23:01 +0200

    Animations 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,10 +7,26 @@ import { switchMap } from 'rxjs/operators';
 import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 import { Feedback,ContactType } from './../shared/feedback';
 import { Comment } from './../shared/Comment';
+import { trigger,state,style,animate,transition} from '@angular/animations';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.css']
+  styleUrls: ['./dishdetail.component.css'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
+
 })
 export class DishdetailComponent implements OnInit {
   dish:Dish ;
@@ -27,6 +43,7 @@ export class DishdetailComponent implements OnInit {
   newDate= this.date.toISOString();
 @ViewChild('fform') FeedbackFormDirective;
 dishcopy:Dish;
+visibility = 'shown';
 
 formErrors = {
   'author': '',
@@ -96,8 +113,8 @@ validationMessages = {
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-    .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+    .pipe(switchMap((params: Params) => {this.visibility ='hidden'; return  this.dishservice.getDish(params['id']);}))
+    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id);this.visibility ='shown'; },
       errmess => this.errMess = <any>errmess )
     }
 
